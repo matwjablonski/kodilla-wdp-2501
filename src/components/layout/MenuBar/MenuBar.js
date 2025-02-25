ca7ca291dd619828bc474e6a660baaa1c746d651
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-02-25 08:39:56 +0100

    Add breakpoints for tablets and smartphones && add icon for mobile menu

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -1,48 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import ProductSearch from '../../features/ProductSearch/ProductSearch';
-
 import styles from './MenuBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const MenuBar = ({ children }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col'>
-          <ProductSearch />
+const MenuBar = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row align-items-center'>
+          <div className='col-6 col-md-12 col-lg-6 d-flex align-items-center'>
+            <ProductSearch />
+            <div className='d-md-none'>
+              <button onClick={toggleMenu} className={styles.menuButton}>
+                <FontAwesomeIcon icon={faBars} />
+              </button>
+            </div>
+          </div>
+          <div className='col-6 col-md-12 col-lg-6'>
+            <div className='d-flex align-items-center'>
+              <div className={`d-none d-md-block ${styles.menu}`}>
+                <ul>
+                  <li>
+                    <a href='#' className={styles.active}>
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href='#'>Furniture</a>
+                  </li>
+                  <li>
+                    <a href='#'>Chair</a>
+                  </li>
+                  <li>
+                    <a href='#'>Table</a>
+                  </li>
+                  <li>
+                    <a href='#'>Sofa</a>
+                  </li>
+                  <li>
+                    <a href='#'>Bedroom</a>
+                  </li>
+                  <li>
+                    <a href='#'>Blog</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={'col-auto ' + styles.menu}>
-          <ul>
-            <li>
-              <a href='#' className={styles.active}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>Furniture</a>
-            </li>
-            <li>
-              <a href='#'>Chair</a>
-            </li>
-            <li>
-              <a href='#'>Table</a>
-            </li>
-            <li>
-              <a href='#'>Sofa</a>
-            </li>
-            <li>
-              <a href='#'>Bedroom</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
-        </div>
+        {isMenuOpen && (
+          <div className={`d-md-none ${styles.dropdownMenu}`}>
+            <ul>
+              <li>
+                <a href='#' className={styles.active}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>Furniture</a>
+              </li>
+              <li>
+                <a href='#'>Chair</a>
+              </li>
+              <li>
+                <a href='#'>Table</a>
+              </li>
+              <li>
+                <a href='#'>Sofa</a>
+              </li>
+              <li>
+                <a href='#'>Bedroom</a>
+              </li>
+              <li>
+                <a href='#'>Blog</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 MenuBar.propTypes = {
   children: PropTypes.node,
