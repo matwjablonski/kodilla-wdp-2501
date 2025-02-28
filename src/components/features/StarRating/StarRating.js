65d513edf0836d03d3836c240750e8fc1b30fbf2
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-28 11:18:38 +0100

    Add myRating and isHovered state and update conditions in StarRating component

--- src/components/features/StarRating/StarRating.js
+++ src/components/features/StarRating/StarRating.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './StarRating.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,12 +6,32 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
 const StarRating = ({ stars }) => {
+  const [myRating, setMyRating] = useState(null);
+  const [isHovered, setIsHovered] = useState(null);
+
+  if (myRating && myRating > 0 && myRating <= 5) stars = myRating;
+  if (isHovered && isHovered > 0 && isHovered <= 5) stars = isHovered;
+
   return (
     <div className={styles.stars}>
       {[1, 2, 3, 4, 5].map(i => (
-        <a key={i} href='#'>
+        <a
+          key={i}
+          href='#'
+          onMouseEnter={() => setIsHovered(i)}
+          onMouseLeave={() => setIsHovered(null)}
+          onClick={e => {
+            e.preventDefault();
+            setMyRating(i);
+          }}
+        >
           {i <= stars ? (
-            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+            <FontAwesomeIcon
+              className={myRating || isHovered ? styles.setRating : ''}
+              icon={faStar}
+            >
+              {i} stars
+            </FontAwesomeIcon>
           ) : (
             <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
           )}
