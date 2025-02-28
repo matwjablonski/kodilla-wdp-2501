18ee0e869fb09d0485738af69eae9aab5031a042
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-02-28 17:46:52 +0100

    Change for PromotedBox && change for section Promoted

--- src/components/features/Promoted/Promoted.js
+++ src/components/features/Promoted/Promoted.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import styles from './Promoted.module.scss';
 import { hotDealsProducts, featuredProducts } from '../../../redux/productsRedux';
 import PromotedBox from '../../common/PromotedBox/PromotedBox';
+import styles from './Promoted.module.scss';
 
 const Promoted = () => {
   const featuredProductsArray = useSelector(featuredProducts);
@@ -14,43 +14,49 @@ const Promoted = () => {
 
   return (
     <section className={styles.promoted}>
-      <div className={styles.row}>
-        {/* Lewa strona (Hot deal) */}
-        <div className={styles.colLeft}>
-          {firstHotDeal ? (
-            <PromotedBox hotDeal={firstHotDeal} />
-          ) : (
-            <p>No hot deals available.</p>
-          )}
-        </div>
+      <div className='container'>
+        {/*
+          .row z klasą .d-flex .align-items-stretch
+          pozwala nam wyrównać wysokość obu kolumn
+        */}
+        <div className='row d-flex align-items-stretch'>
+          {/* Lewa kolumna */}
+          <div className='col-md-4 d-flex flex-column'>
+            {firstHotDeal ? (
+              <PromotedBox hotDeal={firstHotDeal} />
+            ) : (
+              <p>No hot deals available.</p>
+            )}
+          </div>
 
-        {/* Prawa strona (Featured product) */}
-        <div className={styles.colRight}>
-          {featuredProduct ? (
-            <div
-              className={styles.featuredWrapper}
-              style={{ backgroundImage: `url(${featuredProduct.image})` }}
-            >
-              {/* Pasek overlay na środku wysokości */}
-              <div className={styles.overlay}>
-                <h2>
-                  INDOOR <span>FURNITURE</span>
-                </h2>
-                <p>SAVE UP TO 50% OF ALL FURNITURE</p>
-                <button className={styles.shopNow}>SHOP NOW</button>
-              </div>
+          {/* Prawa kolumna */}
+          <div className='col-md-8 d-flex flex-column'>
+            {featuredProduct ? (
+              <div
+                className={styles.featuredWrapper}
+                style={{ backgroundImage: `url(${featuredProduct.image})` }}
+              >
+                {/* Pasek overlay (na środku wysokości) */}
+                <div className={styles.overlay}>
+                  <h2>
+                    INDOOR <span>FURNITURE</span>
+                  </h2>
+                  <p>SAVE UP TO 50% OF ALL FURNITURE</p>
+                  <button className={styles.shopNow}>SHOP NOW</button>
+                </div>
 
-              {/* Strzałki na dole */}
-              <div className={styles.arrows}>
-                <button className={styles.arrowLeft}>&lt;</button>
-                <button className={`${styles.arrowRight} ${styles.active}`}>
-                  &gt;
-                </button>
+                {/* Strzałki na dole */}
+                <div className={styles.arrows}>
+                  <button className={styles.arrowLeft}>&lt;</button>
+                  <button className={`${styles.arrowRight} ${styles.active}`}>
+                    &gt;
+                  </button>
+                </div>
               </div>
-            </div>
-          ) : (
-            <p>No featured product available.</p>
-          )}
+            ) : (
+              <p>No featured product available.</p>
+            )}
+          </div>
         </div>
       </div>
     </section>
