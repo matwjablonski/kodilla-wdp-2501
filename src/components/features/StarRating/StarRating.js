76c15fb3b6e4b3b0400e98740bdb79ce7c43b0f1
Monika Grzanek <grzanekmonika@o2.pl>
2025-02-28 09:47:44 +0100

    Add StarRating component and update ProductBox component

---
+++ src/components/features/StarRating/StarRating.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './StarRating.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
+const StarRating = ({ stars }) => {
+  return (
+    <div className={styles.stars}>
+      {[1, 2, 3, 4, 5].map(i => (
+        <a key={i} href='#'>
+          {i <= stars ? (
+            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+          )}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+StarRating.propTypes = {
+  stars: PropTypes.number,
+};
+
+export default StarRating;
