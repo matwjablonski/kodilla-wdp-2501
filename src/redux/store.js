857a8d3677d3f3b4fc0a3867728c91250d842ad8
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-06 23:57:32 +0100

    Add localStorage and update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import initialState from './initialState';
 import compareFullReducer from './compareFullReducer';
 import alertReducer from './alertReducer';
@@ -8,6 +8,32 @@ import categoriesReducer from './categoriesRedux';
 import productsReducer from './productsRedux';
 import brandsReducer from './brandsRedux';
 import feedbackReducer from './feedbackRedux';
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState) {
+      return JSON.parse(serializedState);
+    }
+  } catch (err) {
+    console.error('Something went wrong...', err);
+  }
+  return {};
+};
+
+const saveStateToLocalStorage = store => next => action => {
+  const result = next(action);
+  const state = store.getState();
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('reduxState', serializedState);
+  } catch (err) {
+    console.error('Something went wrong...', err);
+  }
+  return result;
+};
+
+const persistedState = loadState();
 
 const reducers = {
   alert: alertReducer,
@@ -21,17 +47,24 @@ const reducers = {
 };
 
 Object.keys(initialState).forEach(item => {
-  if (typeof reducers[item] == 'undefined') {
+  if (typeof reducers[item] === 'undefined') {
     reducers[item] = (statePart = null) => statePart;
   }
 });
 
 const combinedReducers = combineReducers(reducers);
 
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? compose(
+      applyMiddleware(saveStateToLocalStorage),
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  : applyMiddleware(saveStateToLocalStorage);
+
 const store = createStore(
   combinedReducers,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  { ...initialState, ...persistedState },
+  storeEnhancers
 );
 
 export default store;
