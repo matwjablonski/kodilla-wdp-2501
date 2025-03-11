867fc1619eed70560dcc31404bacc594185502f5
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-11 10:22:34 +0100

    Update CompareBtn, CompareBarItem, productsRedux components, update test for NewFurniture component

--- src/components/features/CompareBtn/CompareBtn.js
+++ src/components/features/CompareBtn/CompareBtn.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './CompareBtn.module.scss';
 import { useSelector } from 'react-redux';
-import { setCompareStatus } from '../../../redux/productsRedux';
 import clsx from 'clsx';
 import {
   updateFullCompare,
@@ -18,10 +17,13 @@ import Button from '../../common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 
-const CompareBtn = ({ isCompared, id, category }) => {
+const CompareBtn = ({ id, category }) => {
   const dispatch = useDispatch();
   const productsSelected = useSelector(getComparedProducts);
   const fullCompared = useSelector(getComparedFullProducts);
+  const isCompared = productsSelected.find(
+    productSelected => productSelected.id === id
+  );
 
   useEffect(() => {
     if (productsSelected.length === 4) {
@@ -33,14 +35,12 @@ const CompareBtn = ({ isCompared, id, category }) => {
 
   const clickedToCompare = e => {
     e.preventDefault();
-    if (id && !productsSelected.find(product => product.id === id)) {
+    if (id && !isCompared) {
       if (productsSelected.length < 4) {
         dispatch(addToCompare({ id, category }));
-        dispatch(setCompareStatus({ id, isCompared: true }));
       }
     } else {
       dispatch(removeFromCompare(id));
-      dispatch(setCompareStatus({ id, isCompared: false }));
     }
   };
 
@@ -58,7 +58,6 @@ const CompareBtn = ({ isCompared, id, category }) => {
 };
 
 CompareBtn.propTypes = {
-  isCompared: PropTypes.bool,
   id: PropTypes.string,
   category: PropTypes.string,
 };
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -1,5 +1,6 @@
 /* selectors */
 export const getAll = ({ products }) => products;
+
 export const getCount = ({ products }) => products.length;
 
 export const getNew = ({ products }) =>
@@ -25,20 +26,9 @@ export const getBySale = ({ products }) =>
 export const getByRated = ({ products }) =>
   products.filter(product => product.stars > 2);
 
-// actions
-const createActionName = actionName => `app/products/${actionName}`;
-const SET_COMPARE_STATUS = createActionName('SET_COMPARE_STATUS');
-
-// action creators
-export const setCompareStatus = payload => ({ type: SET_COMPARE_STATUS, payload });
-
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
-    case SET_COMPARE_STATUS:
-      return statePart.map(product =>
-        product.id === action.payload.id ? { ...product, ...action.payload } : product
-      );
     default:
       return statePart;
   }
